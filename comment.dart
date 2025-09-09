d4051423faaeb40ebc3cb3d0a4b8f704f2c0b6ec
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2025-09-09 03:27:56 -0700

    comments

---
+++ comment.dart
@@ -0,0 +1,55 @@
+// Malika Xasanboyeva  | 220106
+// Tuesday 14:00
+// Comments
+import 'dart:math';
+
+// Task 2
+void main () {
+  // The variable stores the name of planet we live on
+  String planet = 'Earth';
+  print ('We live on planet $planet .') ;
+  }
+
+
+// Task 3
+
+/*
+Name: Alex
+Date: September 9, 2025
+Description: This is my first Dart function.
+*/
+
+void sayHello() {
+  print('Hello from a function!');
+}
+
+void main() {
+  sayHello();
+}
+
+
+//Task 4
+void main() {
+  // print('This message should not appear.');
+  print('This message should appear.');
+}
+
+
+// Task 5
+
+/// Calculates the area of a circle given its radius.
+double calculateCircleArea(double radius) {
+  return pi * radius * radius;
+}
+
+void main() {
+  print(calculateCircleArea(5.0));
+}
+
+
+// Task 6
+void main() {
+  // This variable stores the speed of light in meters per second.
+  int speedOfLight = 299792458;
+  print(speedOfLight);
+}
