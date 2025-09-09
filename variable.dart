cef72c1c125d85af5f17462eca39e23a77f75f87
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2025-09-09 03:25:50 -0700

    variables

---
+++ variable.dart
@@ -0,0 +1,30 @@
+// Malika Xasanboyeva  | 220106
+// Tuesday 14:00
+// Variables
+
+void main () {
+  // Task 2
+String favCity = 'Tashkent';
+print ('My fav city is $favCity .') ;
+
+// Task 3
+double bookPrice = 15.99;
+print ('The price of the book is $bookPrice USD.') ;
+
+//Task 4
+bool isLearningDart = true;
+print ('Am I learning Dart? The answer is $isLearningDart.') ;
+
+// Task 5
+String weather = 'Sunny';
+int temperature = 32
+print ('Today’s weather is $weather and the temperature is $temperature degrees Celsius') ;
+
+// Task 6
+var numberOfApples = 10;
+print ('Number of apples: $numberOfApples') ;
+
+numberOfApples = 12;
+print ('Number of apples: $numberOfApples') ;
+
+}
