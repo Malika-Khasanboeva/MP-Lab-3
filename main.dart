f52e6bbca537880a3898992d09bf277b90e8f8c2
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2025-09-09 03:25:03 -0700

    main

---
+++ main.dart
@@ -0,0 +1,37 @@
+// Malika Xasanboyeva  | 220106
+// Tuesday 14:00
+// The main Function
+
+void main() {
+  fullName();
+  separateLines();
+  multiline();
+  message();
+  welcomeMessage();
+}
+
+// Task 2
+void fullName() {
+  print('Malika Khasanboeva');
+}
+
+// Task 3
+void separateLines() {
+  print('Welcome to Dart Programming.');
+  print(' Let’s start coding!');
+}
+
+// Task 4
+void multiline() {
+  print('1 Amir Temur Avenue\nTashkent, Uzbekistan\n100000');
+}
+
+// Task 5
+void message() {
+  print('The current year is 2025');
+}
+
+// Task 6
+void welcomeMessage() {
+  print('Welcome, students of INHA University in Tashkent!');
+}
